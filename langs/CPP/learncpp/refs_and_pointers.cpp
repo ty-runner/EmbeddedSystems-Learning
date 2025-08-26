f037524b1a537677923f83f7cf951c5a29f6d741
ty-runner <ty.runner02@gmail.com>
2025-08-26 13:41:49 -0700

    ch12 quiz

---
+++ langs/CPP/learncpp/refs_and_pointers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void sort2_easy(int& x, int& y){
+    if(x > y)
+        std::swap(x,y);
+}
+
+void sort2(int& x, int& y){
+    if(x > y){
+        int swap = x;
+        x = y;
+        y = swap;
+    }
+}
+
+int main()
+{
+    int x = 7;
+    int y = 5;
+
+    std::cout << x << ' ' << y << '\n'; // should print 7 5
+
+    sort2(x, y); // make sure sort works when values need to be swapped
+    std::cout << x << ' ' << y << '\n'; // should print 5 7
+
+    sort2(x, y); // make sure sort works when values don't need to be swapped
+    std::cout << x << ' ' << y << '\n'; // should print 5 7
+
+    return 0;
+}
