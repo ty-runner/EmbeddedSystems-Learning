c9a8d82e526af2527b99b41260035850bf448953
ty-runner <ty.runner02@gmail.com>
2025-08-28 14:46:36 -0700

    euclidian distance constellation problem checker

--- langs/CPP/learncpp/digital_comms_homework_checkers/euclid_distance.cpp
+++ langs/CPP/learncpp/digital_comms_homework_checkers/euclid_distance.cpp
@@ -1,17 +1,55 @@
 #include <iostream>
-/*sum_and_square_5(-1,-1,-1,-1,-1)
-sum_and_square_5(-1,-1,3,-3,3)
-sum_and_square_5(-1,3,-3,3,-1)
-sum_and_square_5(-1,3,1,1,3)
-sum_and_square_5(3,-3,3,-1,-1)
-sum_and_square_5(3,-3,-1,-3,3)
-sum_and_square_5(3,1,1,3,-1)
-sum_and_square_5(3,1,-3,1,3)*/
+#include "sum_5.h"
+
 template <typename T>
-<vector> <T> squared_euclid_distance(vector vector <T> signals, <T> num_signals){
-    int res = 0;
-    <vector> <T> base_signal = signals[0]; //we are finding the euclidian distance of first signal to all other signals
-    for(int i = 1; i<num_signals; i++)
- 
+std::vector<T> vector_diff(const std::vector<T>& a, const std::vector<T>& b) {
+    std::vector<T> res;
+    if (a.size() != b.size()) {
+        std::cerr << "Error: vectors must be the same size\n";
+        return res;
+    }
+    res.resize(a.size());
+    for (size_t i = 0; i < a.size(); i++) {
+        res[i] = a[i] - b[i];
+    }
+    return res;
+}
+
 template <typename T>
-<T> square_and_sum
+std::vector<T> squared_euclid_distance(const std::vector<std::vector<T>>& signals) {
+    std::vector<T> res(signals.size(), 0);
+    if (signals.empty()) return res;
+
+    T total = 0;
+    const std::vector<T>& base_signal = signals[0];
+    for (size_t i = 1; i < signals.size(); i++) {
+        std::vector<T> diff = vector_diff(base_signal, signals[i]);
+        int temp = square_and_sum(diff);
+        total += temp;
+        res[i] = temp;
+    }
+    std::cout << "Total euclid distances from s0 = " << total << std::endl;
+    return res;
+}
+
+int main() {
+    // Populate the 2D vector (8 signals, each of length 5)
+    std::vector<std::vector<int>> signals = {
+        {-1,-1,-1,-1,-1},
+        {-1,-1, 3,-3, 3},
+        {-1, 3,-3, 3,-1},
+        {-1, 3, 1, 1, 3},
+        { 3,-3, 3,-1,-1},
+        { 3,-3,-1,-3, 3},
+        { 3, 1, 1, 3,-1},
+        { 3, 1,-3, 1, 3}
+    };
+
+    std::vector<int> distances = squared_euclid_distance(signals);
+
+    std::cout << "Distances: ";
+    for (int d : distances) std::cout << d << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
