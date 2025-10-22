14b74f5b4124866f05da47b9a8c0942d178264bd
ty-runner <ty.runner02@gmail.com>
2025-10-22 09:05:59 -0700

    tempaltes and function types

--- langs/CPP/learncpp/ch11_function_overload_and_templates/function_overloading.cpp
+++ langs/CPP/learncpp/ch11_function_overload_and_templates/function_overloading.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 template<typename T>
+T mult(T x, int y){
+	return x * y;
+}
+
+template<typename T>
 T add(T x, T y){
 	return x + y;
 }
 
+template<typename T, typename U>
+auto sub(T x, U y){
+	return x - y;
+}
 int main(){
 	int x = 5;
 	float y = 6.7;
 
 	std::cout << add(x,x) << std::endl;
 	std::cout << add(y,y) << std::endl;
-	std::cout << add(x,y) << std::endl;
+	std::cout << mult(y,x) << std::endl;
 
+	std::cout << sub(3, 2) << '\n';
+	std::cout << sub(3.5, 2) << '\n';
+	std::cout << sub(4, 1.5) << '\n';
 	return 0;
 }
