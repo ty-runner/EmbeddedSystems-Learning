ce0fad50d96830168c193488d8082d4731be094e
ty-runner <ty.runner02@gmail.com>
2025-10-25 14:01:32 -0700

    riung bufer in c

--- InterviewPrep/ring_buffer.c
+++ InterviewPrep/ring_buffer.c
@@ -1,75 +1,83 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
+#include "stdio.h"
+#include "stdint.h"
+#include "stdbool.h"
+#define BUFFER_SIZE 5
+typedef struct{ //FIFO behavior
+	int buffer[BUFFER_SIZE];
+	int* buf_start; //start location of buffer region
+	int* buf_end; //end location of buffer region
+	int* data_start;
+	int* data_end;
+} RB;
 
-#define BUFFER_SIZE 8  // fixed size
-
-typedef struct {
-    uint8_t buffer[BUFFER_SIZE];
-    uint8_t *buf_start;
-    uint8_t *buf_end;
-    uint8_t *data_start;  // next byte to read
-    uint8_t *data_end;    // next byte to write
-} RingBuffer;
-
-void rb_init(RingBuffer *rb){
-    rb->buf_start = rb->buffer;
-    rb->buf_end = rb->buf_start + BUFFER_SIZE;
-    rb->data_start = rb->buf_start;
-    rb->data_end = rb->data_start;
+void rb_init(RB* rb){
+	rb->buf_start = rb->buffer;
+	rb->buf_end = rb->buffer + BUFFER_SIZE;
+	rb->data_end = rb->buf_start;
+	rb->data_start = rb->buf_start;
+}
+bool is_empty(RB* rb){
+	return (rb->data_start == rb->data_end);
+}
+bool is_full(RB* rb){
+	//if we increment our write pointer by 1 and is at the boundary, we wrap around
+	return ((rb->data_end + 1 == rb->buf_end) ? rb->buf_start : rb->data_end + 1) == rb->data_start;
 }
 
-bool rb_is_empty(RingBuffer *rb){
-    return(rb->data_start == rb->data_end);
+bool write(RB* rb, int data){
+	if(is_full(rb))
+		return false;
+	*(rb->data_end) = data;
+	rb->data_end = (rb->data_end + 1 == rb->buf_end) ? rb->buf_start : rb->data_end+1;
+	return true;
 }
 
-bool rb_is_full(RingBuffer *rb){
-    return ((rb->data_end +1 == rb->buf_end) ? rb->buf_start : rb->data_end+1) ==rb->data_start;
+bool read(RB* rb, int* data){
+	if(is_empty(rb))
+		return false;
+	*data = *(rb->data_start);
+	rb->data_start = (rb->data_start + 1 == rb->buf_end) ? rb->buf_start : rb->data_start+1;
+	return true;
 }
 
-bool rb_read(RingBuffer *rb, uint8_t* val){
-    //read the data means we are consuming a block of data
-    if(rb_is_empty(rb))
-        return false;
-
-    *val = *rb->data_start;
-    rb->data_start = (rb->data_start+1 == rb->buf_end) ? rb->buf_start : rb->data_start+1;
-    return true;
+void print_buffer(RB* rb){
+	for(int i = 0; i < BUFFER_SIZE; i++){
+		printf("%d\n", (rb->buffer)[i]);
+	}
 }
 
-bool rb_write(RingBuffer *rb, uint8_t val){
-    if(rb_is_full(rb))
-        return false;
+int main(){
+	    RB rb;
+	    rb_init(&rb);
+	    // Write 5 bytes
+	    for (uint8_t i = 0; i < 5; i++)
+		write(&rb, i + 10);
+	    printf("IS FULL? %d\n", is_full(&rb));
+	    print_buffer(&rb);
+	    // Read 3 bytes
+	    //
+	    //
+	    int val;
+	    while (!is_empty(&rb)) {
+		read(&rb, &val);
+		printf("Read: %d\n", val);
+	    }
+	    printf("\n");
+	    for (int i = 0; i < 3; i++) {
+		if(read(&rb, &val))
+			printf("Read: %d\n", val);
+		else
+			printf("EMPTY\n");
+	    }
+	    printf("\n");
+	    // Write 4 more bytes (wraps around)
+	    for (uint8_t i = 0; i < 4; i++)
+		write(&rb, i + 20);
 
-    *rb->data_end = val;
-    rb->data_end = (rb->data_end + 1 == rb->buf_end) ? rb->buf_start : rb->data_end+1;
-    return true;
+	    // Read all remaining bytes
+	    while (!is_empty(&rb)) {
+		read(&rb, &val);
+		printf("Read: %d\n", val);
+	    }
+	    return 0;
 }
-int main() {
-    RingBuffer rb;
-    rb_init(&rb);
-
-    // Write 5 bytes
-    for (uint8_t i = 0; i < 5; i++)
-        rb_write(&rb, i + 10);
-
-    // Read 3 bytes
-    uint8_t val;
-    for (int i = 0; i < 3; i++) {
-        rb_read(&rb, &val);
-        printf("Read: %d\n", val);
-    }
-
-    // Write 4 more bytes (wraps around)
-    for (uint8_t i = 0; i < 4; i++)
-        rb_write(&rb, i + 20);
-
-    // Read all remaining bytes
-    while (!rb_is_empty(&rb)) {
-        rb_read(&rb, &val);
-        printf("Read: %d\n", val);
-    }
-
-    return 0;
-}
-
