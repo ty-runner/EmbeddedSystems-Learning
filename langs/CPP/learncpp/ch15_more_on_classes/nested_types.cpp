8f0bdc40da34b3bd8ad3c04f8e72dae91e5e8102
ty-runner <ty.runner02@gmail.com>
2025-11-06 13:37:57 -0800

    nested member types

---
+++ langs/CPP/learncpp/ch15_more_on_classes/nested_types.cpp
@@ -0,0 +1,81 @@
+/*#include <iostream>
+
+enum class FruitType
+{
+	apple,
+	banana,
+	cherry
+};
+
+class Fruit
+{
+private:
+	FruitType m_type { };
+	int m_percentageEaten { 0 };
+
+public:
+	Fruit(FruitType type) :
+		m_type { type }
+	{
+	}
+
+	FruitType getType() { return m_type; }
+	int getPercentageEaten() { return m_percentageEaten; }
+
+	bool isCherry() { return m_type == FruitType::cherry; }
+
+};
+
+int main()
+{
+	Fruit apple { FruitType::apple };
+
+	if (apple.getType() == FruitType::apple)
+		std::cout << "I am an apple";
+	else
+		std::cout << "I am not an apple";
+
+	return 0;
+}*/ //BEFORE 
+#include <iostream>
+
+
+//AFTER
+class Fruit
+{
+public:
+    enum FruitType
+    {
+        apple,
+        banana,
+        cherry
+    };
+private:
+
+	FruitType m_type { };
+	int m_percentageEaten { 0 };
+
+public:
+	Fruit(FruitType type) :
+		m_type { type }
+	{
+	}
+
+	FruitType getType() { return m_type; }
+	int getPercentageEaten() { return m_percentageEaten; }
+
+	bool isCherry() { return m_type == FruitType::cherry; }
+
+};
+
+int main()
+{
+	Fruit apple { Fruit::apple };
+
+	if (apple.getType() == Fruit::apple)
+		std::cout << "I am an apple";
+	else
+		std::cout << "I am not an apple";
+
+	return 0;
+}
