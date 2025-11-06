aee36519dcc7ba2f0a4e47ed1a630d5301e2ca12
ty-runner <ty.runner02@gmail.com>
2025-11-06 14:38:46 -0800

    constness with templatized classes

---
+++ langs/CPP/learncpp/ch15_more_on_classes/ch15_q1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+/*
+Write a class template named Triad that has 3 private data members with independent type template parameters. 
+The class should have a constructor, access functions, and a print() member function that is defined outside the class.
+
+The following program should compile and run:
+*/
+template <typename T, typename U, typename V>
+class Triad{
+    private:
+       T m_first {}; 
+       U m_second {}; 
+       V m_third {}; 
+    public:
+        Triad(const T& first, const U& second, const V& third):
+            m_first {first}, m_second {second}, m_third {third}
+        {}
+
+        T& first() { return m_first;};
+        U& second() { return m_second;};
+        V& third() { return m_third;};
+
+        void print() const;
+};
+
+template <typename T, typename U, typename V>
+void Triad<T,U,V>::print() const{
+    std::cout << '[' << m_first << ", " << m_second << ", " << m_third << ']' ;
+}
+int main()
+{
+	Triad<int, int, int> t1{ 1, 2, 3 };
+	t1.print();
+	std::cout << '\n';
+	std::cout << t1.first() << '\n';
+
+	using namespace std::literals::string_literals;
+	const Triad t2{ 1, 2.3, "Hello"s };
+	t2.print();
+	std::cout << '\n';
+
+	return 0;
+}
